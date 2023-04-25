4cc4e1cf942a322e4878611a4d343209f00c2c6e
Melany Gomez <go404756@uaeh.edu.mx>
2023-04-25 18:59:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/materialapp/Costo_con_envio.java
@@ -0,0 +1,14 @@
+package com.example.materialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Costo_con_envio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_costo_con_envio);
+    }
+}
